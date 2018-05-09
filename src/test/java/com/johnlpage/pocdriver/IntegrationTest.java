e60096df42bed60cd74b8eb2f2934971abcddc2e
Mark Baker-Munton <markbm@mongodb.com>
2018-05-09 11:41:44 +0100

    disable integration test

--- src/test/java/com/johnlpage/pocdriver/IntegrationTest.java
+++ src/test/java/com/johnlpage/pocdriver/IntegrationTest.java
@@ -1,7 +1,10 @@
 package com.johnlpage.pocdriver;
 
 import org.apache.commons.cli.ParseException;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import javax.swing.text.Document;
 
 import static org.junit.Assert.fail;
 
@@ -10,12 +13,14 @@ public class IntegrationTest {
     private final static int THREADS = 5;
     private final static int DURATION = 20;
 
+    @Ignore
     @Test
     public void runTemplateTest() {
         String templateFile = "src/test/resources/template.json";
         runGenericTest(String.format("-i50 -u50 -k50 -r50 -t%d -e -nPOCDB.template -d%d --template %s", THREADS, DURATION, templateFile));
     }
 
+    @Ignore
     @Test
     public void runBasicTest() {
         runGenericTest(String.format("-i50 -u50 -k50 -r50 -t%d -e -nPOCDB.basic -d%d", THREADS, DURATION));
